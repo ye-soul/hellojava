ed73bd78869bd94dc34addf57d187f69bcf7ab4b
ye-soul <yes8297@gmail.com>
2025-03-27 17:52:09 +0900

    테스트 커밋

---
+++ hellojava/src/ch01/sec06/Hello3.java
@@ -0,0 +1,13 @@
+package ch01.sec06;
+
+public class Hello3 {
+
+	public static void main(String[] args) {
+		int x;
+		x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(result);
+	}
+
+}
---
+++ hellojava/src/ch01/sec06/VariableTest.java
@@ -0,0 +1,11 @@
+package ch01.sec06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char var1 = 'A' + 1 - 2;
+		System.out.println(var1);
+	}
+
+}
